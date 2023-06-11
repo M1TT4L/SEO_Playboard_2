72592685c47e50f9029b38f32358158765a3b7bf
abc <abc@gmail.com>
2023-06-11 11:46:55 +0530

    two icons updayte

--- src/views/admin/Dashboard.js
+++ src/views/admin/Dashboard.js
@@ -32,16 +32,34 @@ export default function Dashboard() {
         </div> */}
 
         {/* <div className="flex flex-wrap justify-center">
-          <div className="w-1/2 sm:w-auto md:w-full lg:w-32 xl:w-3/4">
+          <div className="w-40 sm:w-auto md:w-full lg:w-32 xl:w-3/4">
             <img src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80" 
              alt="..." 
              className="shadow-lg rounded max-w-full h-auto align-middle border-none" />
           </div>
         </div> */}
 
+        <a
+          className="hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+          href="https://github.com"
+          // target="_blank"
+        >
+          <i className="text-blueGray-400 fab fa-amazon text-lg leading-lg " />
+          <span className="lg:hidden inline-block ml-2">Star</span>
+        </a>
+
+        <a
+          className="hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+          href="https://github.com"
+          // target="_blank"
+        >
+          <i className="text-blueGray-400 fab fa-ebay text-lg leading-lg " />
+          <span className="lg:hidden inline-block ml-2">Star</span>
+        </a>
+
         <div className="flex flex-wrap justify-center">
           <div className="relative w-full h-full p-64"> 
-            <iframe title= "firstFrame" className= "relative inset-auto w-full h-full border-none"
+            <iframe title= "firstFrame" className= "relative inset-auto border-none"
               src="https://www.youtube.com/embed/tgbNymZ7vqY">
 
             </iframe>
