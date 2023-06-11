cb5969e7b9a6c0be4b2c6c89c702838182ef667a
CyberHypei <gauravbhardwaj.it23@jecrc.ac.in>
2023-06-11 15:58:51 +0530

    Few

--- src/views/admin/Dashboard.js
+++ src/views/admin/Dashboard.js
@@ -4,7 +4,7 @@ export default function Dashboard() {
   return (
     <>
       <div className="flex flex-wrap">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">  
+        {/* <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">  
           <button className="bg-amber-500 text-white active:bg-amber-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
               Regular
           </button>
@@ -14,15 +14,26 @@ export default function Dashboard() {
           <button className="bg-amber-500 text-white active:bg-amber-600 font-bold uppercase text-sm px-6 py-5 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
               Regular
           </button>
-        </div>
+        </div> */}
 
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">  
+        {/* <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">  
           <button
             className="bg-lightBlue-500 text-white active:bg-amber-600 text-xs font-bold uppercase text-xl px-6 py-4 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
             type="button"
           >
             FlipKart
           </button>
+        </div> */}
+
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">  
+        <a href="https://amazon.in" >
+            <button
+              className="bg-red-600 text-white active:bg-black text-xs font-bold uppercase text-xl px-6 py-4 rounded shadow hover:shadow-lg outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+              type="button"
+            >
+              <i className="text-inherit fab fa-amazon text-5xl leading-lg " />
+            </button>
+          </a>
         </div>
 
         {/* <div className="w-1/2 sm:w-auto md:w-full lg:w-32 xl:w-3/4">  
@@ -39,28 +50,32 @@ export default function Dashboard() {
           </div>
         </div> */}
 
-        <a
-          className="hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-          href="https://github.com"
-          // target="_blank"
-        >
-          <i className="text-blueGray-400 fab fa-amazon text-lg leading-lg " />
-          <span className="lg:hidden inline-block ml-2">Star</span>
-        </a>
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          <a
+            className="hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+            href="https://amazon.in"
+            // target="_blank"
+          >
+            <i className="text-inherit fab fa-amazon text-5xl leading-lg " />
+            <span className="lg:hidden inline-block ml-2">Star</span>
+          </a>
+        </div>
 
-        <a
-          className="hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-          href="https://github.com"
-          // target="_blank"
-        >
-          <i className="text-blueGray-400 fab fa-ebay text-lg leading-lg " />
-          <span className="lg:hidden inline-block ml-2">Star</span>
-        </a>
+        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+          <a
+            className="hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+            href="https://amazon.in"
+            // target="_blank"
+          >
+            <i className=" fab fa-ebay text-5xl leading-lg " />
+            <span className="lg:hidden inline-block ml-2">Star</span>
+          </a>
+        </div>
 
         <div className="flex flex-wrap justify-center">
           <div className="relative w-full h-full p-64"> 
             <iframe title= "firstFrame" className= "relative inset-auto border-none"
-              src="https://www.youtube.com/embed/tgbNymZ7vqY">
+              src="https://www.amazon.in">
 
             </iframe>
           </div>
